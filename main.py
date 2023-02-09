b4f8dc0d2841adbb906a9f844c88a77818adfbd7
PanSingh0047 <112228377+PanSingh0047@users.noreply.github.com>
2023-02-09 14:52:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-# Encryption Algorithm
-
-
-# step 3 converting ASCII to binary
 def decimal(num):
-    x = "{0:b}".format(int(n))
+    x = "{:08b}".format(int(num))
     return x
+def split(x):
+    # step 4
+    Element_1 = int(x/10000)
+    Element_2 = x - Element_1*10000
+    return Element_1, Element_2
+def twoscomplement(num1, num2):
     
+    return num1
+
 if __name__ == '__main__':
-    #step1 to convert into character into ascii code
-c=input("enter the character that u need to convert into it's ascii value: ")
-num1=ord(c)+100
-x = decimal(num1)
+    # step1 to convert into character into ascii code
+    c = input("enter the character that u need to convert into it's ascii value: ")
+    num1 = ord(c) + 100
+    x = int(decimal(num1))
+    Element = split(x)
+    complementElement = twoscomplement(Element[0], Element[1])
+    print(Element[0])
+    print(Element[1])
+    print(complementElement)
+    print(x)
