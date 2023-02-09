e9da18c5991dada99884bb8e4ffd3d4f154b6832
PanSingh0047 <112228377+PanSingh0047@users.noreply.github.com>
2023-02-09 12:49:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,11 @@
 
 # step 3 converting ASCII to binary
 def decimal(num):
-    if num >= 1:
-        decimal(num//2)
-    print(num % 2, end='')
-
+    x = "{0:b}".format(int(n))
+    return x
     
 if __name__ == '__main__':
     #step1 to convert into character into ascii code
 c=input("enter the character that u need to convert into it's ascii value: ")
 num1=ord(c)+100
- decimal(num1)
+x = decimal(num1)
