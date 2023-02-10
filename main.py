e096a38b84a36427207eb7751cfb4c4a827babc1
PanSingh0047 <112228377+PanSingh0047@users.noreply.github.com>
2023-02-10 21:44:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ def decimal(num):
     x = "{:08b}".format(int(num))
     return x
 def split(x):
-    # step 4
     Element_1 = int(x/10000)
     Element_2 = x - Element_1*10000
     return Element_1, Element_2
-def twoscomplement(num1, num2):
-    
-    return num1
+def twoscomplement(val):
+    val = str(val)
+    num1 = int(val, 2)
+    val = ~num1 
+    val = "{:04b}".format(int(val))
+    return val
+def Concatenate(num1, num2):
+    temp = int(str(num1) + str(num2))
+    return temp
 
 if __name__ == '__main__':
     # step1 to convert into character into ascii code
@@ -16,8 +21,10 @@ if __name__ == '__main__':
     num1 = ord(c) + 100
     x = int(decimal(num1))
     Element = split(x)
-    complementElement = twoscomplement(Element[0], Element[1])
-    print(Element[0])
-    print(Element[1])
-    print(complementElement)
+    complementElement1 = twoscomplement(Element[0])
+    complementElement2 = twoscomplement(Element[1])
+    print(complementElement1)
+    print(complementElement2)
+    Concatenated_num = Concatenate(complementElement1, complementElement2)
+    print(Concatenated_num)
     print(x)
